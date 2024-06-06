1957d81fc8935dd625f72e8acdbfb6c65e0e881b
Enzo Caldora <ecaldora@gmail.com>
2024-06-06 14:18:49 +0200

    feat: :sparkles: Todo List Management App
    Add TodoList Application

---
+++ src/app/tasks/tasks.components.ts
@@ -0,0 +1,41 @@
+import {Component, Input} from '@angular/core';
+import {TaskComponent} from './task/task.component';
+import {NewTaskComponent} from "./new-task/new-task.component";
+import { TasksService } from "./tasks.service";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [
+    TaskComponent,
+    NewTaskComponent
+  ],
+  templateUrl: './tasks.components.html',
+  styleUrl: './tasks.components.css'
+})
+export class TasksComponents {
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService){
+
+  }
+
+
+
+  get selectedUserTasks(){
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onCompleteTask(id: string) {
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
